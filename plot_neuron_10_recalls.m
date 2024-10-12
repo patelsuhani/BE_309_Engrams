8896247195981a1209c8ab513e44ad267e918f2a
Suhani Patel <patelsuhani07@gmail.com>
2024-10-12 18:23:49 -0400

    feat: script to plot engrams for a letter being recalled 10 times over a 10 second period

---
+++ plot_neuron_10_recalls.m
@@ -0,0 +1,38 @@
+% Load the data from session2_a_misfire_06.mat
+load('session2_a_misfire_06.mat');  % This will load the 'neuron_network_imaging' variable
+
+% Define parameters
+[num_timepoints, num_neurons] = size(neuron_network_imaging);
+
+% Each cycle is 10 seconds, which is 10,000 milliseconds
+cycle_length_ms = 10000;  % 10 seconds in milliseconds
+num_cycles = 10;    % Total number of cycles
+points_per_cycle = num_timepoints / num_cycles;  % Assuming evenly distributed points per cycle
+
+% Specify the neuron you want to plot (for example, neuron 33)
+neuron_index = 42;  % Change this to plot a different neuron
+
+% Extract the fluorescence data for the specified neuron
+neuron_signal = neuron_network_imaging(:, neuron_index);
+
+% Loop over each cycle and plot
+for cycle_num = 1:num_cycles
+    % Get the start and end indices for the current cycle
+    start_idx = (cycle_num - 1) * points_per_cycle + 1;
+    end_idx = cycle_num * points_per_cycle;
+    
+    % Extract the signal for the current cycle
+    cycle_signal = neuron_signal(start_idx:end_idx);
+    
+    % Create the time vector for the current cycle in milliseconds
+    T_cycle = linspace((cycle_num-1) * cycle_length_ms, cycle_num * cycle_length_ms, points_per_cycle);
+    
+    % Plot the signal for the current cycle
+    figure;
+    plot(T_cycle, cycle_signal, 'b-', 'LineWidth', 1.5);
+    xlabel('Time (milliseconds)');
+    ylabel('Fluorescence Intensity');
+    title(['Fluorescence of Neuron ' num2str(neuron_index) ' - Cycle ' num2str(cycle_num)]);
+    xlim([(cycle_num-1)*cycle_length_ms cycle_num*cycle_length_ms]);  % Set x-axis limits for each cycle in milliseconds
+    grid on;
+end
